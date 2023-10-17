927d6d2c7623da2f7e0cbbd53a06582f060303bc
Jason_xy <53471811+Jason-xy@users.noreply.github.com>
2023-10-18 02:22:30 +0800

    <feat>: add gui frame

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
 #!/usr/bin/env python3
 from config.ProgramConfig import ProgramConfig
 from pipeline.pipeline import pipeline
+from gui.GUI import GUI
+import argparse
+import sys
 import os
 
-def main():
+def argsParser(argv):
+    parser = argparse.ArgumentParser(description='CyberReader')
+    parser.add_argument('--auto_test', action='store_true', help='auto_test without GUI')
+    args = parser.parse_args(argv)
+    return args
+
+def auto_test():
     config = ProgramConfig(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config.yaml'))
     pipeline(config).process()
 
+def main(argv):
+    args = argsParser(argv)
+    if args.auto_test:
+        auto_test()
+    else:
+        config = ProgramConfig(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config.yaml'))
+        pipe = pipeline(config)
+        gui = GUI(pipe)
+        gui.run()
+
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
